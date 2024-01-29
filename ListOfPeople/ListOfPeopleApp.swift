9ddb98f0abf12d6ebd148af39e1464a00283091a
Juliano Lofy Junior <juliano.junior@havan.com.br>
2024-01-29 14:46:55 -0300

    Initial Commit

---
+++ ListOfPeople/ListOfPeopleApp.swift
@@ -0,0 +1,17 @@
+//
+//  ListOfPeopleApp.swift
+//  ListOfPeople
+//
+//  Created by Juliano Lofy Junior on 29/01/24.
+//
+
+import SwiftUI
+
+@main
+struct ListOfPeopleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
